72efb06fab9f09bacdb60c08470eaf91e8fbd0dc
Condoriano <15719920+jd4883@users.noreply.github.com>
2022-02-15 09:20:37 +0000

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "access_modes" { type = list(string) }
+variable "name" { type = string }
+variable "namespace" { type = string }
+variable "size" { type = string }
+variable "storage_class_name" { type = string }
