208817baf6b4def870f12111379460125a7a67b5
Jacob Dresdale <jacob.dresdale@quantcast.com>
2022-06-19 11:49:23 -0700

    * updates code to integrate vsphere static volume assignments * from testing this will always work cleanly and desirably * variables may be a bit minimal but the kubernetes vsphere integration works flawlessly in testing as long as we are talking ReadWriteOnce access modes (docs indicate this should work for ReadWriteMany but this may be exclusively true for vSAN clusters)

--- resources.tf
+++ resources.tf
@@ -1,13 +1,41 @@
-resource "kubernetes_persistent_volume_claim" "pvc" {
-  metadata {
-    name      = var.name
-    namespace = var.namespace
+resource "vsphere_virtual_disk" "disk" {
+  create_directories = var.create_directories
+  datacenter         = var.datacenter
+  datastore          = var.datastore
+  size               = var.size
+  type               = var.disk_type
+  vmdk_path          = local.vmdk_path
+  lifecycle {
+    ignore_changes = [
+      type,
+      datacenter,
+      create_directories,
+    ]
   }
+}
+
+resource "kubernetes_persistent_volume" "pv" {
+  metadata { name = var.name }
   spec {
     access_modes = var.access_modes
-    resources {
-      requests = { storage = var.size }
+    capacity     = local.capacity
+    persistent_volume_source {
+      vsphere_volume {
+        fs_type     = var.fs_type
+        volume_path = local.vsphere_volume_path
+      }
     }
-    storage_class_name = var.storage_class_name
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "pvc" {
+  for_each = toset(var.namespaces)
+  metadata {
+    name      = kubernetes_persistent_volume.pv.metadata.0.name
+    namespace = each.value
+  }
+  spec {
+    access_modes = kubernetes_persistent_volume.pv.spec.0.access_modes
+    resources { requests = kubernetes_persistent_volume.pv.spec.0.capacity }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,27 @@
 variable "access_modes" { type = list(string) }
+variable "datastore" { type = string }
 variable "name" { type = string }
-variable "namespace" { type = string }
-variable "size" { type = string }
-variable "storage_class_name" { type = string }
+variable "namespaces" { type = list(string) }
+variable "size" { type = number }
+variable "vmdk_name" { type = string }
+variable "vmdk_path" { type = string }
+
+variable "create_directories" {
+  default = true
+  type    = bool
+}
+
+variable "datacenter" {
+  default = null
+  type    = string
+}
+
+variable "disk_type" {
+  default = "eagerZeroedThick"
+  type    = string
+}
+
+variable "fs_type" {
+  default = "ext4"
+  type    = string
+}
