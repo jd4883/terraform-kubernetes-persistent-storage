c3fffe318923441b5f2119340a00c83de3bf8496
Jacob Dresdale <jacob.dresdale@quantcast.com>
2022-06-19 20:35:46 -0700

    revised again, vmdk is mandatory

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   size                = format("%sGi", var.size)
   capacity            = { storage = local.size }
-  vmdk_path           = join("/", [split(".", var.vmdk_path).0, var.vmdk_name])
+  vmdk_path           = join("/", [split(".", var.vmdk_path).0, join(".", [var.vmdk_name, "vmdk"])])
   vsphere_volume_path = format("[%s] %s", vsphere_virtual_disk.disk.datastore, vsphere_virtual_disk.disk.vmdk_path)
 }
