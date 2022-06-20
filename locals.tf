6fa8bab66269ccc46e220439e30f8c549f63b90d
Jacob Dresdale <jacob.dresdale@quantcast.com>
2022-06-19 20:29:31 -0700

    light revision suspect the vmdk is redundnant but can't find good examples

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   size                = format("%sGi", var.size)
   capacity            = { storage = local.size }
-  vmdk_path           = join("/", [split(".", var.vmdk_path).0, join(".", [var.vmdk_name, "vmdk"])])
+  vmdk_path           = join("/", [split(".", var.vmdk_path).0, var.vmdk_name])
   vsphere_volume_path = format("[%s] %s", vsphere_virtual_disk.disk.datastore, vsphere_virtual_disk.disk.vmdk_path)
 }
